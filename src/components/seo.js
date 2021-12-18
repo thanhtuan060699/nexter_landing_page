d57072c5b6b359e58c0f8da1021b2572c5adffbd
Nomad Tech <tech.nomad1106@gmail.com>
2021-12-18 10:33:43 +0700

    fix

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
-
+import LogoDark from 'assets/logo.svg';
 export default function SEO({
   description = 'Go Nexter english application',
   author = 'Go Nexter',
@@ -48,6 +48,10 @@ export default function SEO({
       {metaData.map(({ name, content }, i) => (
         <meta key={i} name={name} content={content} />
       ))}
+      <meta property="og:type" content="website"/>
+      <meta property="og:type" content="Nexter"/>
+      <meta property="og:description"   content="English Social Nextwork" />
+      <meta property="og:image"  content="http://www.gonexter.com/_next/static/images/logo-9374d9d71c8962cefbe44ee2d0c48233.svg"/>
     </Head>
   );
 }
