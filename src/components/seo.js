64ceb00f86c65e26061099ef61497562d92433fb
Nomad Tech <tech.nomad1106@gmail.com>
2021-12-18 12:01:36 +0700

    f

--- src/components/seo.js
+++ src/components/seo.js
@@ -46,14 +46,18 @@ export default function SEO({
     <Head>
       <title>{title}</title>
       <meta http-equiv="REFRESH" content="1800"/>
-      <meta property="og:type" content="website"/>
-      <meta property="og:rich_attachment" content="true"/>
       <meta name="description" content="Find friends to chat and speak in English any time, any where"/>
       <meta name="googlebot" content="noarchive"/>
       <meta name="robots" content="noarchive"/>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:rich_attachment" content="true"/>
       <meta property="og:image"  content={LogoDark}/>
       <meta property="og:image:width"  content="720"/>
       <meta property="og:image:height"  content="480"/>
+      <meta property="article:published_time" content=""/>
+      <meta property="article:modified_time" content=""/>
+      <meta property="og:url" content="/"/>
       <meta property="og:title"   content="NEXTER - THE SOCIAL NETWORK FOR ENGLISH LEANRERS" />
       <meta property="og:description"   content="Find friends to chat and speak in English any time, any where" />
     </Head>
